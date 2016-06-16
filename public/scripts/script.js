981e0482dfa0870f147fab182777daf5d3723e70
GINI TYSON <ginityson@yahoo.com>
2016-06-16 09:03:52 -0500

    initial commit

---
+++ public/scripts/script.js
@@ -0,0 +1,64 @@
+$( document ).ready( function(){
+  $( '#addButton' ).on( 'click', function(){
+    // get username from input
+    var newUserName = $( '#usernameIn' ).val();
+    // create object to post
+    var newUser={
+      "username": newUserName,
+      "active": true
+    };// end object
+    // send object to server as a post
+    $.ajax({
+      type: 'POST',
+      url: '/createNew',
+      data: newUser
+    }); // end ajax
+  }); // end addbutton
+
+  $('#getUsers').on('click', function(){
+    // $('#outputDiv').children().remove();
+    $.ajax({
+      type: 'GET',
+      url: '/getUsers',
+      success: function( data ){
+      showUsers( data );
+      } // end success
+    }); //end ajax
+  });
+  function showUsers( users ){
+    console.table( 'in showUsers:' + users );
+    for( i=0; i<users.length; i++ )
+    {
+      var userOut = "<p>" + users[ i ].username + ", active: " + users[ i ].active + " created: " + users[ i ].created + "</p>";
+      $('#outputDiv').append( userOut );
+      var userButton = "<button class='deactivate' data-id='" + users[ i ].id + "'>Deactivate " + users[ i ].username + "</button>";
+      $('#outputDiv').append( userButton );
+
+    } // end for loop
+    $('.deactivate').on('click', function(){
+      console.log('deactivating button');
+      var getID = $(this).attr('data-id');
+
+      $.ajax({
+        type: 'GET',
+        url: '/deactivateUser',
+        data: getID.toString,
+        success: function( data ){
+          // deactivateUser ();
+        } // end success
+      }); //end ajax
+
+      });
+  } // end show users
+
+  // function deactivateUser(){
+  //   console.log('Success!');
+  // }
+
+
+
+
+
+
+
+}); // end jQuery
---
+++ server/app.js
@@ -0,0 +1,59 @@
+var express=require('express');
+var app=express();
+var path=require('path');
+var bodyParser=require('body-parser');
+var urlencodedParser=bodyParser.urlencoded( { extended: false } );
+var pg=require('pg');
+// user to connect to the "introToSQL" table on local host
+// postgres must be running and you must have this db name correct
+var connectionString = 'postgres://localhost:5432/users';
+
+// static public folder
+app.use( express.static( 'public' ) );
+
+// base url
+app.get( '/', function( req, res ){
+  console.log( 'at base url' );
+  res.sendFile( path.resolve( 'views/index.html' ) );
+}); // end base url
+
+// creates a new user from the req.body object that is received
+app.post( '/createNew', urlencodedParser, function( req, res ){
+  console.log( 'in POST createNew: ' + req.body.username + " " + req.body.active );
+  pg.connect( connectionString, function( err, client, done ){
+    // "users" is table. username = $1 = req.body.username
+    client.query( 'INSERT INTO users ( username, active, created ) VALUES ( $1, $2, $3 )', [ req.body.username, req.body.active, 'now()' ] );
+  });
+}); // end createNew
+
+// send back all records in users that conform to the query
+app.get( '/getUsers', function( req, res ){
+  console.log( 'in get users' );
+// this wil hold our results
+  var results =[];
+  pg.connect( connectionString, function( err, client, done ){
+    // get all user records and store in "query" variable
+    var query = client.query( 'SELECT * FROM users WHERE active=true ORDER BY id DESC;' );
+    console.log( "query: " + query );
+    // push each row in query into our results array
+    var rows = 0;
+    query.on( 'row', function ( row ){
+      results.push( row );
+    }); // end query push
+    query.on( 'end', function (){
+      return res.json( results );
+    });
+  }); // end connect
+});
+
+app.get('/deactivateUser', function(req,res){
+  pg.connect( connectionString, function( err, client, done ){
+    client.query("UPDATE users SET active=false WHERE active=true AND id=2;");
+
+  });
+});
+
+// spin up server
+app.listen( 8080, 'localhost', function( req, res ){
+  console.log( "server listening on 8080");
+});
